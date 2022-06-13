2734d3e58d7a5ca4e0182c8b78128d03c0a077ba
kamalhammi <106974298+kimokipo@users.noreply.github.com>
2022-06-13 16:06:23 +0200

    Add files via upload

---
+++ ScratchHome.js
@@ -0,0 +1,82 @@
+class ScratchHome {
+    constructor() {
+        this.varScratch = 60;
+    }
+    
+    getInfo() {
+        return {
+            "id": "ScratchHome",
+            "name": "ScratchHome",
+            "blocks": [
+                        {
+                            "opcode": "setColor",
+                            "blockType": "command",
+                            "text": "colorer [objectList] en [colorList] ",
+                            "arguments": {
+                                "objectList": {
+                                    "type": "string",
+                                    "menu": "objectMenu"
+                                },
+                                "colorList": {
+                                    "type": "string",
+                                    "menu": "colorMenu"
+                                }                                
+                            }
+                        },
+                        {
+                            "opcode": "switchOnOff",
+                            "blockType": "command",
+                            "text": "[switchList] le/la [lampList]",
+                            "arguments": {
+                                "switchList": {
+                                    "type": "string",
+                                    "menu": "switchMenu"
+                                },
+                                "lampList": {
+                                    "type": "string",
+                                    "menu": "lampMenu"
+                                },
+                            }
+                        },
+                        {
+                            "opcode": 'getVarScratch',
+                            "blockType": "reporter",
+                            "text": "varScratch"
+                        }
+                ],
+            "menus":{
+                "colorMenu":{
+                    "items": ["noir", "bleu", "cyan", "gris", "vert", "magenta", "rouge", "blanc", "jaune"]
+                },
+                "objectMenu":{
+                    "items": ["Lit bébé(827158385)", "Table de nuit(1682517865)", "Lit 140x190(826012514)", "Applique murale(759297095)", "Lampadaire(1180091932)", "Source lumineuse blanche(1346157213)", "Suspension(296454286)"]
+                },
+                "switchMenu":{
+                    "items": ["Allumer", "Eteindre"]
+                },
+                "lampMenu":{
+                    "items": ["Applique murale(759297095)", "Lampadaire(1180091932)", "Source lumineuse blanche(1346157213)", "Suspension(296454286)"]
+                }
+            }
+        };
+    }
+    
+    setColor({objectList,colorList}) {
+        this.varScratch = 30;
+    }
+    
+    switchOnOff({switchList,lampList}) {
+        this.varScratch = 90;
+        
+    }
+    /**
+     * Get the current varScratch.
+     * @return {number} - the current varScratch, in beats per minute.
+     */
+    getVarScratch () {
+            return this.varScratch;
+    }
+}
+
+Scratch.extensions.register(new ScratchHome())
+
