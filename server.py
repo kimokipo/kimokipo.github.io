bc11df099e9598b9ada06096511fc769bffedd92
kamalhammi <106974298+kimokipo@users.noreply.github.com>
2022-06-22 13:58:04 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,22 @@
 import asyncio
- 
 import websockets
 import jpysocket
 import socket
+import unidecode 
  
 # create handler for each connection
  
 async def handler(websocket, path):
     while True:
 	    data = await websocket.recv()
-	 
+	    print(data)
 	    host='localhost' #Host Name
 	    port=2022    #Port Number
 	    s=socket.socket() #Create Socket
 	    s.connect((host,port)) #Connect to socket
-	    msgsend=jpysocket.jpyencode(f"GET /{data} HTTP/1") #Encript The Msg
+	    msgenv = f"GET /{data} HTTP/1"
+	    msgenvsansaccent = unidecode.unidecode(msgenv) 
+	    msgsend=jpysocket.jpyencode(msgenvsansaccent) #Encript The Msg
 	    s.send(msgsend) #Send Msg
 	    msgrecv=s.recv(1024) #Recieve msg
 	    msgrecv=jpysocket.jpydecode(msgrecv) #Decript msg
@@ -30,6 +32,5 @@ start_server = websockets.serve(handler, "localhost", 8000)
 asyncio.get_event_loop().run_until_complete(start_server)
  
 asyncio.get_event_loop().run_forever()
-
 
 
